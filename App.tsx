a6b2d9cdc982d759be805320c88fcacd3621c457
GABRIELLA CAMPOS DE ALCANTARA <gabriella.2924@aluno.pr.senac.br>
2026-06-10 16:51:02 -0300

    Iniciando nova tela, MenuScreen.tsx, além de chamar stack no App para importar a navegação de tela, aplicando novos styles e colocando imgs em arrays criado no MenuScreen.tsx, além de importar todas as funcionalidades que vamos usar futuramente na tela

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen, { RootStackParamList } from './HomeScreen'
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen, { RootStackParamList } from './screens/HomeScreen'
+import MenuScreen from './screens/MenuScreen';
+ 
 const Stack = createNativeStackNavigator<RootStackParamList>();
-
+ 
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator
-            initialRouteName='Home'
-            screenOptions={{ headerShown: false }}
-            >
-
+             initialRouteName='Home'
+                    screenOptions={{ headerShown: false }}
+            >                                                                          
                 <Stack.Screen name='Home' component={HomeScreen} />
-
+                <Stack.Screen name='Menu' component={MenuScreen} />
+ 
             </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+        </NavigationContainer>            
+    );                          
+}                                                                                                      
+ 
+ 
+ 
